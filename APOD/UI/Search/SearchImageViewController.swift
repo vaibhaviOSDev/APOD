227ea745527a4afbbe08634143b99177bd088189
Vaibhav <vaibhav5thfeb@gmail.com>
2022-02-09 00:55:53 +0530

    The user is able to select a date of this choice via Data Picker in the prescribed format as per the date format required for the API

--- APOD/UI/Search/SearchImageViewController.swift
+++ APOD/UI/Search/SearchImageViewController.swift
@@ -9,10 +9,55 @@ import UIKit
 
 class SearchImageViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    
+    /// Text Field for  the Date
+    @IBOutlet private weak var dateTextField: UITextField!
+    /// Button for executing the search action
+    @IBOutlet private weak var searchButton: UIButton!
+
+    // MARK:  Properties
+        
+    /// Picker view for the date selection
+    private lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.maximumDate = Date()
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        return datePicker
+    }()
+
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
+        self.view.backgroundColor = .white
+        setUpPickerView()
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    // MARK: - IBAction
+    
+    @IBAction func searchButtonPressed() {
 
+    }
+    // MARK: - Date Picker Events
+    
+    @objc private func onDateSelection() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        dateTextField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    @objc private func cancelDateSelection() {
+        self.view.endEditing(true)
+    }
+    private func setUpPickerView() {
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(onDateSelection))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDateSelection))
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        dateTextField.inputAccessoryView = toolbar
+        dateTextField.inputView = datePicker
+    }
 }
